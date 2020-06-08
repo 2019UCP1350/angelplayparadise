a11552fc655e896dc4943dec379edb6f102d5145
Manvir Singh <2019UCP1350@mnit.ac.it>
2020-06-08 16:43:18 +0530

    first commit

--- gallery.js
+++ gallery.js
@@ -1,16 +1,53 @@
-var j=1;
-showgallery();
-function showgallery()
-{
-	for(i=1;i<=4;i++)
-	{	
-		var d=i+j;
-		if(d>93)
-		{
-			d=1;
-		}
-		document.getElementById("gallery"+i).src="images/christmas2018/1 ("+d+").jpg";
+const galleryimage=document.querySelectorAll(".gallery-slide img");
+const galleryslide=document.querySelector(".gallery-slide");
+const previousbtn=document.getElementById("gallery-pre");
+const nextbtn=document.getElementById("gallery-next");
+var size=galleryimage[0].clientWidth;
+//galleryslide.Width=size*4;
+console.log(size);
+//size=size/4;
+window.onresize=function(){
+	galleryresize();
+	resize();
 	}
-	j=(j+1)%94;
-	setTimeout(showgallery,5000);
+var counter=1;
+function galleryresize()
+{
+	size=galleryslide.clientWidth;
+	//size=size/4;
+}
+nextbtn.addEventListener('click',function(){gallerynext();});
+function gallerynext()
+{
+	if (counter>=galleryimage.length-4) return;
+	galleryslide.style.transition="transform 0.7s ease-in-out";
+	counter++;
+	galleryslide.style.transform="translateX("+(-size*counter)+"px)";
+}
+
+previousbtn.addEventListener('click',()=>{
+	if (counter<=0) return;
+	galleryslide.style.transition="transform 0.7s ease-in-out";
+	counter--;
+	galleryslide.style.transform="translateX("+(-size*counter)+"px)";
+});
+galleryslide.addEventListener('transitionend',()=>{
+	if(galleryimage[counter].id=="lastclone")
+	{
+		galleryslide.style.transition="none";
+		counter=galleryimage.length-6;
+		galleryslide.style.transform="translateX("+(-size*counter)+"px)";
+	}
+	if(galleryimage[counter].id=="firstclone")
+	{
+		galleryslide.style.transition="none";
+		counter=1;
+		galleryslide.style.transform="translateX("+(-size*counter)+"px)";
+	}
+});
+nextimagegallery();
+function nextimagegallery()
+{
+	gallerynext();
+	setTimeout(nextimagegallery,4000);
 }
